621d690c4592c0b139f2c7029a00416467baecbc
devavijayan <devavijayan47@gmail.com>
2023-05-24 04:35:58 -0700

    todo_list

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import  AddTaskReducer from './AddTask';
+ 
+export const store = configureStore({
+    reducer:{
+        addTask: AddTaskReducer
+    }
+}) 
